f487e2c0a23e11099a376d988b9044855220f4a5
Aparna Jha <aparnajha482@gmail.com>
2018-02-26 16:33:50 +0530

    integers omitted in new name.

--- Secret_Message_Decoder.py
+++ Secret_Message_Decoder.py
@@ -5,10 +5,16 @@ def decode_message():  # opening files in directory
     file_list=os.listdir(r"secretMessage")
     #print(file_list)
     saved_path = os.getcwd()
-   # print("current working directory is"+saved_path)
+    # print("current working directory is" +saved_path)
     os.chdir(r"secretMessage")
-    saved_path=("new working directory is"+ saved_path)
-    print("new working directory is" + saved_path)
+    saved_path=("new working directory is" + saved_path)
+    # print("new working directory is" + saved_path)
 
+    #renaming file
+    for file_name in file_list:
+        old_name = file_name
+        new_name = file_name.1strip("123456789")
+        print("Old Name:"+ old_name)
+        print("New Name:"+ new_name)
 
 decode_message()
