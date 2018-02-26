2693da104626fa2bec160f273390f904e6c9c84e
Aparna Jha <aparnajha482@gmail.com>
2018-02-26 16:13:19 +0530

    find current directory.

--- Secret_Message_Decoder.py
+++ Secret_Message_Decoder.py
@@ -6,8 +6,6 @@ def decode_message():  # opening files in directory
     #print(file_list)
     saved_path = os.getcwd()
     print("current working directory is"+saved_path)
-    os.chdir(r"secretMessage")
-    saved_path+os.getcwd()
-    print ("new working directory"+ saved_path)
+
 
 decode_message()
