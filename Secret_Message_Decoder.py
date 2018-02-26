4d9748bde7196f665801681ab3dfd1eac0d2cda7
Aparna Jha <aparnajha482@gmail.com>
2018-02-26 16:22:21 +0530

    working directory changed.

--- Secret_Message_Decoder.py
+++ Secret_Message_Decoder.py
@@ -5,7 +5,7 @@ def decode_message():  # opening files in directory
     file_list=os.listdir(r"secretMessage")
     #print(file_list)
     saved_path = os.getcwd()
-    print("current working directory is"+saved_path)
+   # print("current working directory is"+saved_path)
     os.chdir(r"secretMessage")
     saved_path=("new working directory is"+ saved_path)
     print("new working directory is" + saved_path)
