58638c8aa7400d23acf1e1e5edb7c4216dfebe43
Aparna Jha <aparnajha482@gmail.com>
2018-02-26 16:36:47 +0530

    secret message decoded.

--- Secret_Message_Decoder.py
+++ Secret_Message_Decoder.py
@@ -2,19 +2,21 @@ import os
 
 
 def decode_message():  # opening files in directory
-    file_list=os.listdir(r"secretMessage")
-    #print(file_list)
+    file_list = os.listdir(r"secretMessage")
+    # print(file_list)
     saved_path = os.getcwd()
     # print("current working directory is" +saved_path)
     os.chdir(r"secretMessage")
-    saved_path=("new working directory is" + saved_path)
+    saved_path = ("new working directory is" + saved_path)
     # print("new working directory is" + saved_path)
 
-    #renaming file
+    # renaming file
     for file_name in file_list:
         old_name = file_name
-        new_name = file_name.1strip("123456789")
-        print("Old Name:"+ old_name)
-        print("New Name:"+ new_name)
+        new_name = file_name.lstrip("123456789")
+        print("Old Name:" + old_name)
+        print("New Name:" + new_name)
+        os.rename(old_name, new_name)
+
 
 decode_message()
